a169926432e25266372ed633c513ae933ab9ddcf
dolel13 <dolel@outlook.com>
2014-07-23 19:22:07 +0300

    Update cosine_similarity.py

--- algorithms/cosine_similarity.py
+++ algorithms/cosine_similarity.py
@@ -1,14 +1,6 @@
 from math import sqrt
 
 
-users = {
-    'Daniel': {'m300': 2.5, 'Lord of the Rings': 3.5, 'Django Unchained': 3.5, 'Madagascar': 4.5},
-    'Lloyd': {'m300': 4.5, 'Lord of the Rings': 4.5, 'Django Unchained': 4.0, 'Madagascar': 4.0},
-    'Steve': {'m300': 4.5, 'Lord of the Rings': 4.5, 'Django Unchained': 4.5, 'Madagascar': 4.0},
-    'Lenny': {'m300': 4.0, 'Lord of the Rings': 4.0, 'Django Unchained': 3.5, 'Madagascar': 3.5}
-}
-
-
 def cosine_similarity(rating1, rating2):
     dot_product = 0
     length_of_vector_x = 0
@@ -22,13 +14,3 @@ def cosine_similarity(rating1, rating2):
         return 0
     else:
         return dot_product / ((sqrt(length_of_vector_x)) * sqrt(length_of_vector_y))
-
-
-if __name__ == "__main__":
-    print "Cosine Similarity between Users"
-    print "\nDaniel and Lloyd: ", cosine_similarity(users['Daniel'], users['Lloyd'])
-    print "\nDaniel and Steve: ", cosine_similarity(users['Daniel'], users['Steve'])
-    print "\nDaniel and Lenny: ", cosine_similarity(users['Daniel'], users['Lenny'])
-    print "\nLloyd and Steve: ", cosine_similarity(users['Lloyd'], users['Steve'])
-    print "\nLloyd and Lenny: ", cosine_similarity(users['Lloyd'], users['Lenny'])
-    print "\nSteve and Lenny: ", cosine_similarity(users['Steve'], users['Lenny'])
